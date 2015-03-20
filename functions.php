99feaada8bc963e984856113f5fee4b6f0e22a76
unknown <browk276@CE105WD554989S.hillnet.ad.hsd.k12.or.us>
2015-03-20 10:01:35 -0700

    saved unworking functions.php

--- functions.php
+++ functions.php
@@ -6,5 +6,14 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', 
 	get_stylesheet_uri(), 
 	array( 'parent-style' ) );
+		// Load Boostrap Styles
+	 wp_enqueue_style( 'bootstrap', 
+	get_stylesheet_directory_uri(),'/css/boostrap.min.css ');
+	wp_enqueue_style( 'bootstrap-theme', 
+	get_stylesheet_directory_uri(),'/css/boostrap-theme.min.css ');
+		// Load Bootstap js file
+	wp_enqueue_style( 'script-name', 
+	get_stylesheet_directory_uri(),'/js/boostrap.min.js ',
+	array( 'jquery' ) );
 }
 ?>
