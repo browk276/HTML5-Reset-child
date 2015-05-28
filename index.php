6f44ad8a64c11781ecfb59102fff13dd8845abd6
unknown <browk276@CE105WD554989S.hillnet.ad.hsd.k12.or.us>
2015-05-28 09:40:13 -0700

    copied Mr. Winikka's Index file, has errors, as it won't load

--- index.php
+++ index.php
@@ -1,17 +1,54 @@
 <?php
-/*
-*default home page index.php
+/**
+* Template Name: Home Page Template
 */
-get header()
-?>
+get header()?>
+
+<div class="row highlights">
+	<div class="post-highlight col-md-6">
+		<!-- Goal: get most recent post from the Dry Rot category-->
+		<?php $the_query = new WP_Query('category_name=dry_rot&posts_per_page=1');
+			if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();
+		?>
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<?php the excerpt(); ?>
+		<?php endwhile; endif; ?>
+	</div>
+	<div class="post-highlight col-md-6">
+		<!-- Goal: get the most recent post from Water Leaks category-->
+		<?php $the_query = new WP_Query('category_name=water_leaks&posts_per_page=1');
+			if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();
+		?>
+		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<?php the_excerpt(); ?>
+		<?php endwhile; endif; ?>
+	</div>
+</div>
+
 <div class="row">
-	<div class="spacer col-md-1"></div>
 	<div class="content col-md-8">
-		<h2>IIINNNDDDEEEXXX!!!</h2>
-			
+		<?php if (have_posts()) : while (have_posts()): the_post(); ?>
+			<article class="post" id="post-<?php the_ID(); ?>">
+				<h2><?php the_title(); ?></h2>
+				<div class="entry">
+					<?php the_content(); ?>
+				</div>
+				<?php edit_post_link(__('Edit this page', 'html5reset'), '<p>', '</p>'); ?>
+			</article>
+		<?php endwhile; endif; ?>
+		
+		<!-- create one more of the N most recent posts -->
+		<?php $the_query = new WP_Query('posts_per_page=4');
+			if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();
+		?>
+		<article class="post" id="post-<?php the_ID(); ?>">
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<?php the_excerpt(); ?>
+			<?php endwhile; endif; ?>
+		</article>
 	</div>
-	<div class="sidebar col-md-2">
+	<div class="sidebar col-md-4">
 		<?php get_sidebar(); ?>
-	</div>
+		</div>
 </div>
 <?php get_footer(); ?>
